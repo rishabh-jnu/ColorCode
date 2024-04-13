ec5db5034f15bbd2cfab72324fc08db1c813bd71
Your Name <rishabhyadav70070@gmail.com>
2024-04-13 12:49:25 +0530

    ColorCode Project

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const getcolor = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215)
+    //console.log(randomNumber);
+    const randomCode = "#" + randomNumber.toString(16);
+    console.log(randomNumber, randomCode);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+    navigator.clipboard.writeText(randomCode)
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getcolor
+)
+
+getcolor();
